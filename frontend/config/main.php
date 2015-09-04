4a9d96e37e2b9365a9d4fd5f20ed349187e9a7c3
Ngoc Luong-Bao <ngoclb@elarion.com>
2015-09-04 15:29:36 +0700

    Initial commit with project structure

---
+++ frontend/config/main.php
@@ -0,0 +1,76 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-backend',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+            'layout' => 'right-menu',
+            'mainLayout' => '@app/views/layouts/main.php',
+            'menus' => [
+                'assignment' => [
+                    'label' => 'Grant Access' // change label
+                ],
+                'route' => null, // disable menu
+            ],
+        ],
+        'gridview' => [
+            'class' => '\kartik\grid\Module',
+        ],
+    ],
+    'components' => [
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        // 'view' => [
+        //     'theme' => [
+        //         'pathMap' => [
+        //            '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
+        //         ],
+        //     ],
+        // ],
+        'assetManager' => [
+            'bundles' => [
+                'dmstr\web\AdminLteAsset' => [
+                    'skin' => 'skin-purple',
+                ],
+            ],
+        ],
+        // 'as access' => [
+        //     'class' => 'mdm\admin\components\AccessControl',
+        //     'allowActions' => [
+        //         'site/*',
+        //         'admin/*',
+        //         // 'some-controller/some-action',
+        //         // The actions listed here will be allowed to everyone including guests.
+        //         // So, 'admin/*' should not appear here in the production, of course.
+        //         // But in the earlier stages of your development, you may probably want to
+        //         // add a lot of actions here until you finally completed setting up rbac,
+        //         // otherwise you may not even take a first step.
+        //     ]
+        // ],
+    ],
+    'params' => $params,
+];
