2806b10a063166166f6547751c99ecd7c3508773
quanld <quanld@elarion.com>
2015-09-21 18:20:28 +0700

    no zoom

--- frontend/views/layouts/mobile/main.php
+++ frontend/views/layouts/mobile/main.php
@@ -15,8 +15,8 @@ MobileAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-  <meta charset="<?= Yii::$app->charset ?>"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="<?= Yii::$app->charset ?>"/>  
+  <meta name="viewport" id="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
   <?= Html::csrfMetaTags() ?>
   <title><?= Html::encode($this->title) ?></title>
   <?php $this->head() ?>
