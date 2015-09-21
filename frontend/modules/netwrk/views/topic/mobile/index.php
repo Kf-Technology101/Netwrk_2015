f7bd72f15b917f9e8ad50539c45fe301a71def0a
quanld <quanld@elarion.com>
2015-09-21 18:28:45 +0700

    [Fixbug] show list data

--- frontend/modules/netwrk/views/topic/mobile/index.php
+++ frontend/modules/netwrk/views/topic/mobile/index.php
@@ -20,9 +20,15 @@
        </table> 
     </div>
     <div class="container">
-        <div id="item_list_post" data-img="<?= Url::to('@web/img/icon/timehdpi.png'); ?>"></div>
-        <div id="item_list_view" data-img="<?= Url::to('@web/img/icon/timehdpi.png'); ?>"></div>
-        <div id="item_list_topic" data-img="<?= Url::to('@web/img/icon/timehdpi.png'); ?>"></div>
+        <div id="item_list_post" data-img="<?= Url::to('@web/img/icon/timehdpi.png'); ?>">
+            <p class="no-data">There is no data available yet</p>
+        </div>
+        <div id="item_list_view" data-img="<?= Url::to('@web/img/icon/timehdpi.png'); ?>">
+            <p class="no-data">There is no data available yet</p>
+        </div>
+        <div id="item_list_topic" data-img="<?= Url::to('@web/img/icon/timehdpi.png'); ?>">
+            <p class="no-data">There is no data available yet</p>
+        </div>
         <script id="topic_list" type="text/x-underscore-template" >
             <% _.each(topices,function(topic){ %>
                 <div class="item"> 
