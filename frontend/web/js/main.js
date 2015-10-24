f25768190cdac270d587f2247d1b13bdc649fb38
quanld <quanld@elarion.com>
2015-10-24 16:36:20 +0700

    resize map

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -4,7 +4,7 @@ function get_size_window(){
 
 function set_size_map(w,h){
   var menu_h = $('.menu_top').height();
-  $('#googleMap').css('height',h - menu_h);
+  $('#googleMap').css({'height': h - menu_h,'width': w});
 }
 
 function set_position_btn_meet(w,h){
