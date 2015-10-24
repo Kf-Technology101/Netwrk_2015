6e042c5cc2a5dd3d3aef7520449dac310ddcdc55
quanld <quanld@elarion.com>
2015-10-24 16:52:51 +0700

    set height maps

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -4,7 +4,7 @@ function get_size_window(){
 
 function set_size_map(w,h){
   var menu_h = $('.menu_top').height();
-  $('#googleMap').css({'min-height': h - menu_h,'width': w});
+  $('#googleMap').css({'height': h - menu_h,'width': w});
   // $('.map_content').css({'height': h - menu_h,'width': w});
 }
 
