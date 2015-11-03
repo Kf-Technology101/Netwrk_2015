251673add6f3a483bd0d2130950a2c7934942f1f
quanld <quanld@elarion.com>
2015-11-03 22:05:57 +0700

    Change migration data

--- frontend/modules/netwrk/migrations/m150904_100101_initial_cities.php
+++ frontend/modules/netwrk/migrations/m150904_100101_initial_cities.php
@@ -17,18 +17,6 @@ class m150904_100101_initial_cities extends \yii\db\Migration
             'lat' => Schema::TYPE_FLOAT,
             'lng' => Schema::TYPE_FLOAT,
         ], $tableOptions);
-
-        $this->execute("INSERT INTO {{%city}} (name, lat, lng)
-            VALUES ('Bloomington', 39.165325, -86.526386),
-            ('Evansville', 37.971559, -87.571090),
-            ('Fort Wayne', 41.079273, -85.139351),
-            ('Indianapolis', 39.768403, -86.158068),
-            ('Kokomo', 40.486427, -86.133603),
-            ('West Lafayette', 40.425869, -86.908066),
-            ('Muncie', 40.193377, -85.386360),
-            ('Richmond', 39.828937, -84.890238),
-            ('South Bend', 41.676355, -86.251990),
-            ('Terre Haute', 39.466703, -87.413909);");
     }
 
     public function down()
--- frontend/modules/netwrk/migrations/m150923_020101_post_example.php
+++ frontend/modules/netwrk/migrations/m150923_020101_post_example.php
@@ -23,14 +23,6 @@ class m150923_020101_post_example extends \yii\db\Migration
 
         $current_date = date('Y-m-d H:i:s');
         $security = Yii::$app->security;
-
-        $this->execute("INSERT INTO {{%post}} (title, content, topic_id, user_id, created_at, updated_at) VALUES
-            ('Post Ex1','This blog Post Ex1',1,2,'{$current_date}','{$current_date}'),
-            ('Post Ex2','This blog Post Ex2',1,2,'{$current_date}','{$current_date}'),
-            ('Post Ex3','This blog Post Ex3',1,2,'{$current_date}','{$current_date}'),
-            ('Post Ex4','This blog Post Ex4',1,2,'{$current_date}','{$current_date}'),
-            ('Post Ex5','This blog Post Ex5',1,3,'{$current_date}','{$current_date}');
-        ");
     }
 
     public function down()
