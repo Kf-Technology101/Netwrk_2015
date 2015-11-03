0879325e736d29207081541bc420b2dda59b4448
quanld <quanld@elarion.com>
2015-11-03 17:15:32 +0700

    fix migration post table

--- frontend/modules/netwrk/migrations/m151102_060101_add_column_view_count_to_post.php
+++ frontend/modules/netwrk/migrations/m151102_060101_add_column_view_count_to_post.php
@@ -10,23 +10,15 @@ class m151102_060101_add_column_view_count_to_post extends \yii\db\Migration
         if ($this->db->driverName === 'mysql') {
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
         }
-        
-        $this->createTable('post', [
-            'id' => Schema::TYPE_PK,
-            'title' => Schema::TYPE_STRING . '(128) NOT NULL',
-            'content' => Schema::TYPE_TEXT . ' NOT NULL',
-            'topic_id' => Schema::TYPE_INTEGER . '(11)',
-            'user_id' => Schema::TYPE_INTEGER . '(11) NOT NULL',
-            'created_at' => Schema::TYPE_DATETIME,
-            'updated_at' => Schema::TYPE_DATETIME,
-            'view_count' => Schema::TYPE_FLOAT,
-            'brilliant_count' => Schema::TYPE_FLOAT,
-            'comment_count' => Schema::TYPE_FLOAT,
-        ], $tableOptions);
+        $this->addColumn('post', 'view_count', Schema::TYPE_INTEGER);
+        $this->addColumn('post', 'brilliant_count', Schema::TYPE_INTEGER);
+        $this->addColumn('post', 'comment_count', Schema::TYPE_INTEGER);
     }
 
     public function down()
     {
-        $this->dropTable('post');
+        $this->dropColumn('post', 'view_count', Schema::TYPE_INTEGER);
+        $this->dropColumn('post', 'brilliant_count', Schema::TYPE_INTEGER);
+        $this->dropColumn('post', 'comment_count', Schema::TYPE_INTEGER);
     }
 }
