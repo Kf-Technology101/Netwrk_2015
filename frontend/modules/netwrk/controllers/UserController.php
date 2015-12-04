b7c118d0a9e7124b293d5dc6b794bb5bb164a68f
quanld <quanld@elarion.com>
2015-12-04 18:04:31 +0700

    add signup desktop

--- frontend/modules/netwrk/controllers/UserController.php
+++ frontend/modules/netwrk/controllers/UserController.php
@@ -11,6 +11,12 @@ use frontend\modules\netwrk\models\UserSettings;
 
 use frontend\modules\netwrk\models\forms\LoginForm;
 
+use yii\web\Response;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\widgets\ActiveForm;
+use yii\db\ActiveQuery;
+
 use yii\helpers\Url;
 
 class UserController extends BaseController
@@ -36,12 +42,57 @@ class UserController extends BaseController
     }
 
     public function actionIndex(){
-        var_dump(Yii::$app->user);
-        die;
         //return $this->render($this->getIsMobile() ? 'mobile/login' : '');
     }
 
     public function actionSignup(){
         return $this->render($this->getIsMobile() ? 'mobile/signup' : '');
+    }
+
+    public function actionRegister()
+    {
+
+        $user = new User();
+        $profile = new Profile();
+
+        // load post data
+        $post = Yii::$app->request->post();
+
+        if ($user->load($post)) {
+
+            // ensure profile data gets loaded
+            $profile->load($post);
+
+            // validate for ajax request
+            if (Yii::$app->request->isAjax) {
+                Yii::$app->response->format = Response::FORMAT_JSON;
+                return ActiveForm::validate($user, $profile);
+            }
+
+            // validate for normal request
+            if ($user->validate() && $profile->validate()) {
+
+                // perform registration
+                $role = Yii::$app->getModule("netwrk")->model("Role");
+                $user->setRegisterAttributes($role::ROLE_USER, Yii::$app->request->userIP)->save(false);
+                $profile->setUser($user->id)->save(false);
+                $this->afterRegister($user);
+
+                // set flash
+                // don't use $this->refresh() because user may automatically be logged in and get 403 forbidden
+                $successText = "Successfully registered [$user->getDisplayName()]";
+                $guestText = "";
+                if (Yii::$app->user->isGuest) {
+                    $guestText =  " - Please check your email to confirm your account";
+                }
+                Yii::$app->session->setFlash("Register-success", $successText . $guestText);
+            }
+        }
+
+        // render
+        return $this->render("signup", [
+            'user'    => $user,
+            'profile' => $profile,
+        ]);
     }
 }
