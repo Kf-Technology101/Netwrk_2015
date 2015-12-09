e9d0b68adada41d5f26c6e65e800c49c1b6a56d2
tainb <tainb@elarion.com>
2015-12-09 15:13:00 +0700

    [Fix] Chat Inbox: fix chat menu hover and image resize

--- frontend/modules/netwrk/views/default/partial/chat_inbox.php
+++ frontend/modules/netwrk/views/default/partial/chat_inbox.php
@@ -2,8 +2,8 @@
 <div id='chat_inbox' class='chat-inbox' >
 	<!-- Nav tabs -->
 	<ul class="nav nav-tabs chat-inbox-tab" role="tablist">
-		<li role="presentation" class=" col-xs-6"><a href="#chat_private" aria-controls="chat_private" role="tab" data-toggle="tab">Chats</a></li>
-		<li role="presentation" class="active col-xs-6"><a href="#chat_dicussion" aria-controls="chat_dicussion" role="tab" data-toggle="tab">Discussions</a></li>
+		<li role="presentation" class=" col-xs-6"><a href="#chat_private" aria-controls="chat_private" role="tab" data-toggle="tab"><span>Chats</span></a></li>
+		<li role="presentation" class="active col-xs-6"><a href="#chat_dicussion" aria-controls="chat_dicussion" role="tab" data-toggle="tab"><span>Discussions</span></a></li>
 	</ul>
 
 	<!-- Tab panes -->
@@ -16,7 +16,7 @@
 							<img class='img_avatar' src='<?= Url::to('@web/img/icon/no_avatar.jpg'); ?>' />
 						</span>
 						<div class='title-description-user'>
-							<div class='title-chat-inbox'>#General Dogs</div>
+							<div class='title-chat-inbox'>General Dogs</div>
 							<div class='description-chat-inbox'>Lorem ipsum dolor sit amet, consectet Lorem ipsum dolor sit amet, consectet Lorem ipsum dolor sit amet, consectet</div>
 						</div>
 						<span class='time-chat-inbox'><i class='fa fa-clock-o'></i> 1 hr</span>
