1a1b4c53cfed0964023ba004e98760e4199b9608
Shailesh <shailesh.bonzer@gmail.com>
2016-02-11 11:16:20 +0530

    Group table migration quick fixes
    Group table migration primary key issue fixes.
    Also added migration for user group.

--- frontend/modules/netwrk/migrations/m160204_023941_group_col.php
+++ frontend/modules/netwrk/migrations/m160204_023941_group_col.php
@@ -1,12 +1,18 @@
 <?php
 
 use yii\db\Migration;
+use yii\db\Schema;
 
 class m160204_023941_group_col extends Migration {
 
     public function up() {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+
         $this->createTable('group', [
-            "id" => "int(11) NOT NULL AUTO_INCREMENT",
+            "id" => Schema::TYPE_PK,
             "user_id" => "int(11) NOT NULL",
             "city_id" => "int(11) DEFAULT NULL",
             "latitude" => "double NOT NULL",
@@ -14,9 +20,9 @@ class m160204_023941_group_col extends Migration {
             "address" => "varchar(256) NOT NULL",
             "name" => "varchar(256) NOT NULL",
             "permission" => "int(11) NOT NULL",
-            "created_at" => "datetime NOT NULL",
-            "updated_at" => "datetime NOT NULL"
-        ]);
+            "created_at" => Schema::TYPE_TIMESTAMP,
+            "updated_at" => Schema::TYPE_TIMESTAMP
+        ], $tableOptions);
 
         $this->addColumn('topic', 'group_id', $this->integer());
         $this->addColumn('ws_messages', 'group_id', $this->integer());
