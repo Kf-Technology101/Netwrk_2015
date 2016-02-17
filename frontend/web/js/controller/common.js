7232a73fc00c019af225762072b6063c49c1cc1e
Yogesh <yogesh.bonzer@gmail.com>
2016-02-17 16:38:20 +0530

    WIP Chat and Meet button functionality on mobile

--- frontend/web/js/controller/common.js
+++ frontend/web/js/controller/common.js
@@ -4,24 +4,50 @@
 var Common = {
     contexts : {
         'boxNavigation': '.box-navigation',
-        'btnExplore': '.box-navigation .btn-explore'
+        'btnExplore': '.box-navigation .btn-explore',
+        'chatInboxNavBtnMobile': '#chat_inbox_nav_btn_mobile',
+        'btnNavMeetMobile' : '#btn_nav_meet_mobile'
     },
 
     initialize: function() {
-        Common.OnClickExplore();
+        Common.eventClickExplore();
+        Common.eventClickChatInboxBtnMobile();
+        Common.eventClickMeetBtnMobile();
     },
 
     /* On clicking map btn in nav, it will redirect to default home on mobile */
-    OnClickExplore: function(){
+    eventClickExplore: function(){
         var target = $(Common.contexts.btnExplore);
+        
         target.unbind();
         target.on('click',function(e){
             if(isMobile){
                 sessionStorage.show_landing = 1;
                 window.location.href = baseUrl + "/netwrk/default/home";
-            }else{
-                console.log('common.OnClickExplore');
             }
+        });
+    },
+    eventClickChatInboxBtnMobile: function() {
+        var target = $(Common.contexts.chatInboxNavBtnMobile);
+        target.unbind();
+        target.on('click',function(){
+            sessionStorage.url = window.location.href;
+            Common.OnClickChatInboxMobile();
+        });
+    },
+    OnClickChatInboxMobile: function() {
+        if ( window.location.href == baseUrl + "/netwrk/chat-inbox") {
+            window.location.href = sessionStorage.url;
+        } else {
+            window.location.href = baseUrl+ "/netwrk/chat-inbox";
+        }
+    },
+    eventClickMeetBtnMobile: function(){
+        var target = $(Common.contexts.btnNavMeetMobile);
+
+        target.unbind();
+        target.on('click',function(){
+            Meet.showUserMeetMobile();
         });
     }
 };
