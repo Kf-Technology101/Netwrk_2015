210d6088ba8a5ed41e50971e3a1daabdba6c06ac
Yogesh <yogesh.bonzer@gmail.com>
2016-02-18 18:26:33 +0530

    WIP on navigation redesign on mobile

--- frontend/modules/netwrk/views/user/mobile/userinfo.php
+++ frontend/modules/netwrk/views/user/mobile/userinfo.php
@@ -1,11 +1,11 @@
 <?php use yii\helpers\Url;?>
 <script id="account_nav_dropdown" type="text/x-underscore-template" >
-    <div id='account_nav_wrapper' class="btn-group profile-dropdown" role="group">
+    <div id='account_nav_wrapper' class="btn-group profile-dropdown dropup" role="group">
         <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
             <i class="navigation-icon fa fa-user"></i>
             <div class="navigation-text">Profile</div>
         </button>
-        <ul class="dropdown-menu">
+        <ul class="dropdown-menu dropdown-menu-right">
             <li class='avatar-dropdown-menu'><a><i class="fa fa-tachometer"></i> Dashboard</a></li>
             <li class='avatar-dropdown-menu'><a><i class="fa fa-user"></i> Profile</a></li>
             <li class='avatar-dropdown-menu sign-out'><a href="<?= Url::base(true); ?>/netwrk/user/logout"><i class="fa fa-sign-out"></i> Sign Out</a></li>
