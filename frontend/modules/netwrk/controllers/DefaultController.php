53a74d1573dd20c55ec8bc5884c6de9b7a3547f2
Yogesh <yogesh.bonzer@gmail.com>
2016-04-11 17:59:13 +0530

    Highlight Zip code area which entered on cover page

--- frontend/modules/netwrk/controllers/DefaultController.php
+++ frontend/modules/netwrk/controllers/DefaultController.php
@@ -800,4 +800,35 @@ class DefaultController extends BaseController
         return $hash;
     }
 
+    public function actionGetZipBoundries() {
+        $result = array();
+
+        $zip_code = $_GET['zip_code'];
+
+        $url = "http://boundaries.io/geographies/postal-codes?search=".$zip_code;
+
+        $headers[] = 'Accept: application/json';
+        $headers[] = 'Connection: Keep-Alive';
+        $headers[] = 'Content-type: application/x-www-form-urlencoded;charset=UTF-8';
+
+        //Initiate curl
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        // Disable SSL verification
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // Will return the response, if false it print the response
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // Set the url
+        curl_setopt($ch, CURLOPT_URL,$url);
+
+        // Execute
+        $result = curl_exec($ch);
+        // Closing
+        curl_close($ch);
+
+        $data = (array)(json_decode($result));
+        $data = (json_encode($data[0]->geometry->coordinates));
+
+        die($data);
+    }
 }
