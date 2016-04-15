b9438c2367d198bc4c05da78ffae7bdd9cfb8fae
Shailesh <shailesh.bonzer@gmail.com>
2016-04-15 10:30:31 +0530

    Updated map color styles to gray white

--- frontend/web/js/controller/map.js
+++ frontend/web/js/controller/map.js
@@ -21,51 +21,32 @@
 		zoomBlueDot: 18,
 		remove_poi : [
 			{
-				"featureType":"landscape",
-				"stylers":[
-					{"hue":"#FFBB00"},
-					{"saturation":43.400000000000006},
-					{"lightness":37.599999999999994},
-					{"gamma":1}
+				stylers: [
+					{ hue: "#0078ff" },
+					{ saturation: -20 }
 				]
 			},{
-				"featureType":"road.highway",
-				"stylers":[
-					{"hue":"#FFC200"},
-					{"saturation":-61.8},
-					{"lightness":45.599999999999994},
-					{"gamma":1}
+				featureType: "road",
+				elementType: "labels",
+				stylers: [
+					{ visibility: "off" }
 				]
 			},{
-				"featureType":"road.arterial",
-				"stylers":[
-					{"hue":"#FF0300"},
-					{"saturation":-100},
-					{"lightness":51.19999999999999},
-					{"gamma":1}
+				featureType: "road",
+				elementType: "geometry",
+				stylers: [
+					{ lightness: 100 }
 				]
 			},{
-				"featureType":"road.local",
-				"stylers":[
-					{"hue":"#FF0300"},
-					{"saturation":-100},
-					{"lightness":52},
-					{"gamma":1}
+				featureType: "poi",
+				stylers: [
+					{ visibility: "off" }
 				]
 			},{
-				"featureType":"water",
-				"stylers":[
-					{"hue":"#0078FF"},
-					{"saturation":-13.200000000000003},
-					{"lightness":2.4000000000000057},
-					{"gamma":1}]
-			},{
-				"featureType":"poi",
-				"stylers":[
-					{"hue":"#00FF6A"},
-					{"saturation":-1.0989010989011234},
-					{"lightness":11.200000000000017},
-					{"gamma":1}
+				featureType: "administrative",
+				elementType: "geometry.stroke",
+				stylers: [
+					{ color: "#5888ac" }
 				]
 			}
 		],
