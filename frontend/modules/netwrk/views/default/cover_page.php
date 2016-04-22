a7730805815c7344d2c934b882c7ebf42ee7802c
Shailesh <shailesh.bonzer@gmail.com>
2016-04-22 16:11:32 +0530

    Removed auto complete from cover page

--- frontend/modules/netwrk/views/default/cover_page.php
+++ frontend/modules/netwrk/views/default/cover_page.php
@@ -5,7 +5,7 @@
 	<span class="error hidden">Sorry my friend, you shall not pass</span><br/>
 	<img src="<?= Url::to('@web/img/icon/netwrk-logo-blue_large.png'); ?>">
 	<div class="input-group">
-        <input type="text" class="form-control cover-input" id="cv-password" placeholder="Please enter zip code or city">
+        <input type="password" class="form-control cover-input" id="cv-password" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;What is the password?">
         <span class="input-group-addon"><i class="fa fa-key"></i></span>
     </div>
     <?= $this->render('@frontend/modules/netwrk/views/search/cover-result') ?>
