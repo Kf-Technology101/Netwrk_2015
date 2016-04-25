f08f53680cfa79a4f513cd5ad195214a95a54ec8
Shailesh <shailesh.bonzer@gmail.com>
2016-04-25 12:46:55 +0530

    Removed "Communities" word from profile sections

--- frontend/modules/netwrk/views/default/partial/profile.php
+++ frontend/modules/netwrk/views/default/partial/profile.php
@@ -19,7 +19,7 @@
 
             <div class="profile-activity-wrapper">
                 <section class="fav-communities-wrapper">
-                    <div class="activity-header pull-left">Following Communities </div>
+                    <div class="activity-header pull-left">Followed</div>
                     <div class="seperator-line pull-right">
                         <hr>
                     </div>
@@ -31,7 +31,7 @@
                 </article>
 
                 <section class="recent-communities-wrapper">
-                    <div class="activity-header pull-left">Recent Communities </div>
+                    <div class="activity-header pull-left">Recent</div>
                     <div class="seperator-line pull-right">
                         <hr>
                     </div>
