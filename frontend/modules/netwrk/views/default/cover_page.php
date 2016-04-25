8e10f4db105356229ce5d2032d1babf844fef2f7
Shailesh <shailesh.bonzer@gmail.com>
2016-04-25 12:49:32 +0530

    Added code to hide cover page input on page load

--- frontend/modules/netwrk/views/default/cover_page.php
+++ frontend/modules/netwrk/views/default/cover_page.php
@@ -3,8 +3,8 @@
 ?>
 <div id="cover-page">
 	<span class="error hidden">Sorry my friend, you shall not pass</span><br/>
-	<img src="<?= Url::to('@web/img/icon/netwrk-logo-blue_large.png'); ?>">
-	<div class="input-group">
+	<img src="<?= Url::to('@web/img/icon/netwrk-logo-blue_large.png'); ?>" onload="$('#inputGroup').removeClass('hide');">
+	<div class="input-group hide" id="inputGroup">
         <input type="text" class="form-control cover-input" id="cv-password" placeholder="Please enter zip code or city">
         <span class="input-group-addon"><i class="fa fa-key"></i></span>
     </div>
