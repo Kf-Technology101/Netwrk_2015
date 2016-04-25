bda03e30b10b3890289adcf14d151a2f98dc1cf2
Shailesh <shailesh.bonzer@gmail.com>
2016-04-25 15:02:16 +0530

    Added fade in effect when info window opens

--- frontend/web/js/controller/map.js
+++ frontend/web/js/controller/map.js
@@ -312,6 +312,11 @@
 			        // infowindow.setContent(e[0]);
 			        clearTimeout(Map.timeout);
 			        infowindow.open(Map.map, this);
+
+					var iw_container = $(".gm-style-iw").parent();
+					iw_container.stop().hide();
+					iw_container.fadeIn(800);
+
 			        Map.onhoverInfoWindow(e.id,marker);
 			        Map.OnEventInfoWindow(e);
 		        });
