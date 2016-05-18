7eda8de64bd50b41acf6c5dc2d7f09abe1d1b88f
Shailesh <shailesh.bonzer@gmail.com>
2016-05-18 17:41:49 +0530

    WIP on Group pagination

--- frontend/modules/netwrk/controllers/GroupController.php
+++ frontend/modules/netwrk/controllers/GroupController.php
@@ -11,6 +11,7 @@ use frontend\modules\netwrk\models\UserInvitation;
 use frontend\modules\netwrk\models\City;
 use frontend\modules\netwrk\models\Topic;
 use frontend\modules\netwrk\models\Post;
+use yii\data\Pagination;
 use yii\base\Exception;
 use Yii;
 
@@ -285,8 +286,12 @@ class GroupController extends BaseController {
 
     public function actionGetGroupsByUser()
     {
-        $filter = $_GET['filter'];
+        $filter = isset($_GET['filter']) ? $_GET['filter'] : '';
+        $pageSize = isset($_GET['size']) ? $_GET['size'] : '';
+        $page = isset($_GET['page']) ? $_GET['page'] : '';
+
         $currentUserId = isset($_GET['user']) ? $_GET['user'] : Yii::$app->user->id;
+
         switch ($filter) {
             case 'post':
                 $order = ['post_count' => SORT_DESC];
@@ -319,9 +324,13 @@ class GroupController extends BaseController {
             ->where($params)
             ->orderBy($order);
 
-        $totalCount = $groups->count();
+        $countQuery = clone $groups;
+        $totalCount = $countQuery->count();
 
-        $groups = $groups->all();
+        $pages = new Pagination(['totalCount' => $totalCount,'pageSize'=>$pageSize,'page'=> $page - 1]);
+        $groups = $groups->offset($pages->offset)
+            ->limit($pages->limit)
+            ->all();
 
         /*$sql = $groups->createCommand()->getRawSql();
         echo($sql);
