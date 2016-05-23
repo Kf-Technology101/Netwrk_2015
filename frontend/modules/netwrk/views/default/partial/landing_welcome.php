4d9d21c0c8b6b14e68992fe1e93f11381a4ee67c
Shailesh <shailesh.bonzer@gmail.com>
2016-05-23 11:41:15 +0530

    Added functionality to display welcome modal on desktop.
    This welcome modal is only displayed once when user first time comes to
    system from cover page.
    It will not popup every time when front page loads.

---
+++ frontend/modules/netwrk/views/default/partial/landing_welcome.php
@@ -0,0 +1,15 @@
+<?php use yii\helpers\Url; ?>
+<div class="modal" id='modal_landing_welcome' role='dialog'>
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<h1>Welcome to your Community</h1>
+			</div>
+		</div>
+	</div>
+</div>
