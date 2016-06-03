8063786f6d54bb45fb7ef52bd53eed1a1af3d369
Shailesh <shailesh.bonzer@gmail.com>
2016-06-03 12:19:38 +0530

    Hide deleted post from chat discussion

--- frontend/modules/netwrk/controllers/PostController.php
+++ frontend/modules/netwrk/controllers/PostController.php
@@ -306,34 +306,36 @@ class PostController extends BaseController
             if ($messages) {
 
                 foreach ($messages as $key => $message) {
-                    $user_photo = User::findOne($message->post->user_id)->profile->photo;
-                    if ($user_photo == null) {
-                        $image = 'img/icon/no_avatar.jpg';
-                    } else {
-                        $image = 'uploads/' . $message->post->user_id . '/' . $user_photo;
-                    }
+                    if($message->post->status != -1){
+                        $user_photo = User::findOne($message->post->user_id)->profile->photo;
+                        if ($user_photo == null) {
+                            $image = 'img/icon/no_avatar.jpg';
+                        } else {
+                            $image = 'uploads/' . $message->post->user_id . '/' . $user_photo;
+                        }
 
-                    $currentVote = Vote::find()->where('user_id= ' . $currentUser . ' AND post_id= ' . $message->post->id)->one();
-                    $num_comment = UtilitiesFunc::ChangeFormatNumber($message->post->comment_count ? $message->post->comment_count + 1 : 1);
-                    $num_brilliant = UtilitiesFunc::ChangeFormatNumber($message->post->brilliant_count ? $message->post->brilliant_count : 0);
-                    $num_date = UtilitiesFunc::FormatTimeChat($message->post->created_at);
-                    $item = [
-                        'id' => $message->post->id,
-                        'post_title' => $message->post->title,
-                        'post_content' => $message->post->content,
-                        'topic_id' => $message->post->topic_id,
-                        'topic_name' => $message->post->topic->title,
-                        'city_id' => $message->post->topic->city_id,
-                        'city_name' => $message->post->topic->city->name,
-                        'title' => $message->post->title,
-                        'content' => $message->post->content,
-                        'num_comment' => $num_comment ? $num_comment : 0,
-                        'num_brilliant' => $num_brilliant ? $num_brilliant : 0,
-                        'avatar' => $image,
-                        'update_at' => $num_date,
-                        'real_update_at' => $message->post->chat_updated_time ? $message->post->chat_updated_time : $message->post->created_at
-                    ];
-                    array_push($data, $item);
+                        $currentVote = Vote::find()->where('user_id= ' . $currentUser . ' AND post_id= ' . $message->post->id)->one();
+                        $num_comment = UtilitiesFunc::ChangeFormatNumber($message->post->comment_count ? $message->post->comment_count + 1 : 1);
+                        $num_brilliant = UtilitiesFunc::ChangeFormatNumber($message->post->brilliant_count ? $message->post->brilliant_count : 0);
+                        $num_date = UtilitiesFunc::FormatTimeChat($message->post->created_at);
+                        $item = [
+                            'id' => $message->post->id,
+                            'post_title' => $message->post->title,
+                            'post_content' => $message->post->content,
+                            'topic_id' => $message->post->topic_id,
+                            'topic_name' => $message->post->topic->title,
+                            'city_id' => $message->post->topic->city_id,
+                            'city_name' => $message->post->topic->city->name,
+                            'title' => $message->post->title,
+                            'content' => $message->post->content,
+                            'num_comment' => $num_comment ? $num_comment : 0,
+                            'num_brilliant' => $num_brilliant ? $num_brilliant : 0,
+                            'avatar' => $image,
+                            'update_at' => $num_date,
+                            'real_update_at' => $message->post->chat_updated_time ? $message->post->chat_updated_time : $message->post->created_at
+                        ];
+                        array_push($data, $item);
+                    }
                 }
 
                 // return strtotime($data[0]['real_update_at']) - strtotime($data[1]['real_update_at']);die;
