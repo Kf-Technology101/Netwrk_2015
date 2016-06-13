f3aabb0b679d892aa4cf7d75f291e37d6881d619
Shailesh <shailesh.bonzer@gmail.com>
2016-06-13 16:49:52 +0530

    Made followed & recent community design element common

--- frontend/modules/netwrk/views/profile/mobile/index.php
+++ frontend/modules/netwrk/views/profile/mobile/index.php
@@ -279,40 +279,46 @@
     </div>
 </script>
 <script id="profile_fav-communities_template" type="text/x-underscore-template">
-    <div class="fav-communities_list clearfix">
-        <div class="fav-communities-list" id="favoriteCommunities">
+    <div class="communities_list clearfix">
+        <div class="communities-list" id="favoriteCommunities">
             <% if(!_.isEmpty(items)) {%>
-            <% _.each(items, function(item, key){ %>
-            <div class="fav-community">
-                <span class="fav-zip-code pull-left">
-                    <a class="community-modal-trigger"
-                       href="javascript:"
-                       data-lat="<%= item.lat %>"
-                       data-lng="<%= item.lng %>"
-                       data-city-id="<%= item.city_id %>">
-                        <%= item.city_zipcode %>
-                    </a>
-                </span>
-                <span class="fav-action pull-right un-favorite-trigger"
-                      data-object-type="<%= 'city' %>"
-                      data-object-id="<%= item.city_id %>"><i class="fa fa-trash-o"></i>
-                </span>
-            </div>
-            <% }); %>
+                <% _.each(items, function(item, key){ %>
+                    <div class="community">
+                        <span class="zip-code pull-left">
+                            <a class="community-modal-trigger"
+                               href="javascript:"
+                               data-lat="<%= item.lat %>"
+                               data-lng="<%= item.lng %>"
+                               data-city-id="<%= item.city_id %>">
+                                <%= item.city_zipcode %>
+                            </a>
+                        </span>
+                        <span class="community-action pull-right un-favorite-trigger"
+                              data-object-type="<%= 'city' %>"
+                              data-object-id="<%= item.city_id %>"><i class="fa fa-trash-o"></i>
+                        </span>
+                    </div>
+                <% }); %>
             <% } else {%>
-            <div class="alert alert-info">Currently there is no favorite communities</div>
+                <div class="alert alert-info">Currently there is no favorite communities</div>
             <% } %>
         </div>
     </div>
 </script>
 <script id="profile_recent-communities_template" type="text/x-underscore-template">
     <div class="clearfix">
-        <div class="recent-communities-list" id="recentCommunities">
+        <div class="communities-list" id="recentCommunities">
             <% if(!_.isEmpty(items)) {%>
                 <% _.each(items, function(item, key){ %>
-                    <div class="recent-community">
-                        <span class="recent-zip-code pull-left"><a class="community-modal-trigger" href="javascript:" data-city-id="<%= item.city_id %>"><%= item.city_zipcode %></a></span>
-                        <span class="recent-action pull-right remove-recent-trigger"
+                    <div class="community">
+                        <span class="zip-code pull-left">
+                            <a class="community-modal-trigger"
+                               href="javascript:"
+                               data-city-id="<%= item.city_id %>">
+                                <%= item.city_zipcode %>
+                            </a>
+                        </span>
+                        <span class="community-action pull-right remove-recent-trigger"
                               data-log_id="<%= item.log_id %>"
                               data-type="<%= 'city' %>"
                               data-city_id="<%= item.city_id %>"><i class="fa fa-trash-o"></i>
@@ -325,4 +331,3 @@
         </div>
     </div>
 </script>
-
