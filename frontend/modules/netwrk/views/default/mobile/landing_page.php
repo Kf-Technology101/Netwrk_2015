9246ef4ccf9a8ff1ef7a93e7fb70d5e75217248a
Shailesh <shailesh.bonzer@gmail.com>
2016-06-14 10:56:39 +0530

    Added collapse on landing page mobile view

--- frontend/modules/netwrk/views/default/mobile/landing_page.php
+++ frontend/modules/netwrk/views/default/mobile/landing_page.php
@@ -33,94 +33,79 @@
 		 data-title="<%= landing.title %>" data-content="<%= landing.post_content %>">Area Party Line</div>
 </script>
 <script id="landing_page" type="text/x-underscore-template">
-	<div class="top-communities">
-		<div class="top-header">
-			<p class="lp-title">Home Public Centers</p>
-			<!--<p class="lp-description">See whats happening in your area!</p>-->
-		</div>
-		<div class="top-communities-content">
-			<%
-			_.each(landing.top_communities,function(e,i){ %>
-			<div class="communities-row" data-city="<%= e.city_id %>">
-				<div class="com-content">
-					<p class="zipcode"><%= e.zip_code %> - <%= (e.office_name != null) ? e.office_name : e.city_name %></p>
-					<p class="subtext">
-						<% _.each(e.top_hashtag,function(d,s){ %>
-						<span><%=d.hashtag %></span>
-						<%})%>
-					</p>
-				</div>
-				<span class="arrow"><i class="fa fa-angle-right"></i></span>
+	<div class="panel-group" id="accordion">
+		<div class="panel panel-default top-communities" id="panelTopCommunities">
+			<div class="panel-heading top-header">
+				<a data-toggle="collapse" data-target="#collapseTopCommunities"
+				   href="javascript:" class="collapsed">
+					<p class="lp-title">Home Public Centers</p>
+					<!--<p class="lp-description">See whats happening in your area!</p>-->
+				</a>
 			</div>
-			<%
-			});
-			%>
-		</div>
-	</div>
-	<div class="top-post">
-		<div class="top-header">
-			<p class="lp-title">On the net today</p>
-			<!--<p class="lp-description">Check out some of the discussions on some of your favorite subjects</p>-->
-		</div>
-		<div class="top-post-content">
-			<%
-				var len_post = landing.top_post.length;
-				_.each(landing.top_post,function(e,i){
-					if(i == len_post - 1){%>
-							<div class="post-row" data-value="<%= e.id %>" data-user="<%= e.user_id %>">
-					<% }else{ %>
-							<div class="post-row" data-value="<%= e.id %>" data-user="<%= e.user_id %>">
-					<% } %>
-							<div class="avatar"><div class="image"><img src="<%= e.photo %>"></div></div>
-
-							<div class="post">
-								<p class="post-title"><%= e.title %></p>
-								<div class="post-content"><%= e.content%></div>
-							</div>
-							<div class="action">
-								<div class="chat"><i class="fa fa-comments"></i>Chat</div>
-
-								<span class="brilliant"><%= e.brilliant_count%></span>
-							</div>
+			<div id="collapseTopCommunities" class="panel-collapse collapse">
+				<div class="panel-body top-communities-content">
+					<%
+					_.each(landing.top_communities,function(e,i){ %>
+					<div class="communities-row" data-city="<%= e.city_id %>">
+						<div class="com-content">
+							<p class="zipcode" ><%= e.zip_code %> - <%= (e.office_name != null) ? e.office_name : e.city_name %></p>
+							<p class="subtext">
+								<% _.each(e.top_hashtag,function(d,s){ %>
+								<span><%=d.hashtag %></span>
+								<%})%>
+							</p>
 						</div>
-			<%
-				});
-			%>
+						<span class="arrow"><i class="fa fa-angle-right"></i></span>
+					</div>
+					<%
+					});
+					%>
+				</div>
+			</div>
+		</div>
 
-			<%
-				var len_topic = landing.top_post.length;
-				_.each(landing.top_topic,function(e,i){
-					if(i == len_topic - 1){ %>
-						<div class="topic-row last-row" data-value="<%= e.id %>">
-					<% }else{ %>
-						<div class="topic-row" data-value="<%= e.id %>">
-					<% } %>
-							<p class="topic-title"><%= e.name %></p>
-							<div class="post-counter">
-								<%= e.post_count %>
-								<span class="arrow"><i class="fa fa-angle-right"></i></span>
-								<i class="fa fa-file-text"></i>
+		<div class="panel panel-default top-post" id="panelTopPosts">
+			<div class="panel-heading top-header">
+				<a data-toggle="collapse" data-target="#collapseTopPosts"
+				   href="javascript:" class="collapsed">
+					<p class="lp-title">On the net today</p>
+					<!--<p class="lp-description">Check out some of the discussions on some of your favorite subjects</p>-->
+				</a>
+			</div>
+			<div id="collapseTopPosts" class="panel-collapse collapse">
+				<div class="panel-body top-post-content">
+					<%
+						var len_post = landing.top_post.length;
+						_.each(landing.top_post,function(e,i){
+						if(i == len_post - 1){%>
+							<div class="post-row" data-value="<%= e.id %>" data-user="<%= e.user_id %>">
+						<% }else{ %>
+							<div class="post-row" data-value="<%= e.id %>" data-user="<%= e.user_id %>">
+						<% } %>
+								<div class="avatar"><div class="image"><img src="<%= e.photo %>"></div></div>
+
+								<div class="post">
+									<p class="post-title"><%= e.title %></p>
+									<div class="post-content"><%= e.content%></div>
+								</div>
+								<div class="action">
+									<div class="chat"><i class="fa fa-comments"></i>Chat</div>
+
+									<span class="brilliant"><%= e.brilliant_count%></span>
+								</div>
 							</div>
-						</div>
-			<%
-				});
-			%>
-		</div>
-	</div>
-	<!--<div class="top-topic">
-		<div class="top-header">
-			<p class="lp-title">Top Topics</p>
-			<p class="lp-description">Browse these topics of conversations</p>
-		</div>
-		<div class="top-topic-content">
-			<%
-				var len_topic = landing.top_post.length;
-				_.each(landing.top_topic,function(e,i){
-					if(i == len_topic - 1){ %>
-							<div class="topic-row last-row" data-value="<%= e.id %>">
-					<% }else{ %>
-							<div class="topic-row" data-value="<%= e.id %>">
-					<% } %>
+						<%
+						});
+					%>
+
+					<%
+						var len_topic = landing.top_post.length;
+						_.each(landing.top_topic,function(e,i){
+						if(i == len_topic - 1){ %>
+							<div class="topic-row last-row" data-value="<%= e.id %>" data-city="<%= e.city_id %>" data-city-name="<%= e.city_name %>">
+						<% }else{ %>
+							<div class="topic-row" data-value="<%= e.id %>" data-city="<%= e.city_id %>" data-city-name="<%= e.city_name %>">
+						<% } %>
 								<p class="topic-title"><%= e.name %></p>
 								<div class="post-counter">
 									<%= e.post_count %>
@@ -128,61 +113,102 @@
 									<i class="fa fa-file-text"></i>
 								</div>
 							</div>
-			<%
-				});
-			%>
-
-		</div>
-	</div>-->
-
-	<% if(!_.isEmpty(landing.feeds)) {%>
-		<div class="favorite-communities">
-			<div class="top-header">
-				<p class="lp-title">Your Followed Areas</p>
+						<%
+						});
+					%>
+				</div>
 			</div>
-			<div class="favorite-communities-content">
-				<% if(!_.isEmpty(landing.feeds)) {%>
-					<% _.each(landing.feeds, function(city_feed, key){ %>
-						<% _.each(city_feed, function(e, key){ %>
-							<% if ((e.is_post == 1)){ %>
-								<div class="feed-row feed-post" data-user="<%= e.user_id %>" data-value="<%= e.id %>" data-city="<%= e.city_id %>" data-topic='<%= e.topic_id %>'>
-									<div class="avatar-poster"><div class="image"><img src="<%= e.photo %>"></div></div>
-									<div class="feed-content">
-										<div class='post'>
-											<div class='post-title'><%= e.title %></div>
-											<div class='post-content'><%= e.content %></div>
-										</div>
-										<span class='post-create-by'>Posted by: <%= e.posted_by %></span>
-										<span class='appear-day'>
-											<% if ((e.appear_day == 'Now')){ %>
-												Just Now
-											<% }else{ %>
-												<%= e.appear_day %> ago
-											<% } %>
-										</span>
-									</div>
-								</div>
+		</div>
+
+		<!--<div class="panel panel-default top-topic" id="panelTopTopics">
+			<div class="panel-heading top-header">
+				<a data-toggle="collapse" data-target="#collapseTopTopics"
+				   href="javascript:" class="collapsed">
+					<p class="lp-title">Top Topics</p>
+					<p class="lp-description">Browse these topics of conversations</p>
+				</a>
+			</div>
+			<div id="collapseTopTopics" class="panel-collapse collapse">
+				<div class="panel-body top-topic-content">
+					<%
+						var len_topic = landing.top_post.length;
+						_.each(landing.top_topic,function(e,i){
+						if(i == len_topic - 1){ %>
+						<div class="topic-row last-row" data-value="<%= e.id %>" data-city="<%= e.city_id %>" data-city-name="<%= e.city_name %>">
 							<% }else{ %>
-								<div class="feed-row feed-topic" data-value="<%= e.id %>" data-city="<%= e.city_id %>" data-city-name='<%= e.city_name %>'>
-									<div class="feed-content">
-										<span class='topic-title'><%= e.title %></span>
-										<span class='topic-create-by'>Topic created by: <%= e.created_by %></span>
-										<span class='appear-day'>
-											<% if ((e.appear_day == 'Now')){ %>
-												Just Now
-											<% }else{ %>
-												<%= e.appear_day %> ago
-											<% } %>
-										</span>
-									</div>
+							<div class="topic-row" data-value="<%= e.id %>" data-city="<%= e.city_id %>" data-city-name="<%= e.city_name %>">
+								<% } %>
+								<p class="topic-title"><%= e.name %></p>
+								<div class="post-counter">
+									<%= e.post_count %>
+									<span class="arrow"><i class="fa fa-angle-right"></i></span>
+									<i class="fa fa-file-text"></i>
 								</div>
-							<% } %>
-						<% }); %>
-					<% }); %>
-				<% } %>
+							</div>
+						<%
+						});
+					%>
+				</div>
 			</div>
-		</div>
-	<% } %>
+		</div>-->
+
+		<% if(!_.isEmpty(landing.feeds)) {%>
+			<div class="panel panel-default favorite-communities" id="panelFavoriteCommunities">
+				<div class="panel-heading top-header">
+					<a data-toggle="collapse" data-target="#collapseFavoriteCommunities"
+					   href="javascript:">
+						<p class="lp-title">Your Followed Areas</p>
+					</a>
+				</div>
+				<div id="collapseFavoriteCommunities" class="panel-collapse collapse in">
+					<div class="panel-body favorite-communities-content">
+
+						<% if(!_.isEmpty(landing.feeds)) {%>
+							<% _.each(landing.feeds, function(city_feed, key){ %>
+								<!--<div class="community-title"><%= key %></div>-->
+								<% _.each(city_feed, function(e, key){ %>
+
+									<% if ((e.is_post == 1)){ %>
+										<div class="feed-row feed-post" data-user="<%= e.user_id %>" data-value="<%= e.id %>" data-city="<%= e.city_id %>" data-topic='<%= e.topic_id %>'>
+											<div class="avatar-poster"><div class="image"><img src="<%= e.photo %>"></div></div>
+											<div class="feed-content">
+												<div class='post'>
+													<div class='post-title'><%= e.title %></div>
+													<div class='post-content'><%= e.content %></div>
+												</div>
+												<span class='post-create-by'>Posted by: <%= e.posted_by %></span>
+												<span class='appear-day'>
+													<% if ((e.appear_day == 'Now')){ %>
+													  Just Now
+													<% }else{ %>
+													  <%= e.appear_day %> ago
+													<% } %>
+												</span>
+											</div>
+										</div>
+									<% }else{ %>
+										<div class="feed-row feed-topic fav-community-topic" data-value="<%= e.id %>" data-city="<%= e.city_id %>" data-city-name='<%= e.city_name %>'>
+											<div class="feed-content">
+												<span class='topic-title'><%= e.title %></span>
+												<span class='topic-create-by'>Topic created by: <%= e.created_by %></span>
+												<span class='appear-day'>
+													<% if ((e.appear_day == 'Now')){ %>
+														Just Now
+													<% }else{ %>
+														<%= e.appear_day %> ago
+													<% } %>
+												</span>
+											</div>
+										</div>
+									<% } %>
+								<% }); %>
+							<% }); %>
+						<% } %>
+					</div>
+				</div>
+			</div>
+		<% } %>
+	</div>
 </script>
 
 <?= $this->render('../partial/landing_welcome');?>
