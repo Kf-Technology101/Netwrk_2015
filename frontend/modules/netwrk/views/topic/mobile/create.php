10cae7ab50d69762d43d5d460c3b464c0aabf7dd
Yogesh <yogesh.bonzer@gmail.com>
2016-06-16 18:41:18 +0530

    Create channel header mobile updates

--- frontend/modules/netwrk/views/topic/mobile/create.php
+++ frontend/modules/netwrk/views/topic/mobile/create.php
@@ -7,7 +7,7 @@
             <span><i class="fa fa-arrow-circle-left"></i> Back </span>
         </div>
         <div class="title_page">
-            <span class="title"><?= $city->zip_code ?> > Build a channel and add lines to it</span>
+            <span class="title"><?= $city->zip_code ?> > Build a channel & add lines to it</span>
         </div>
     </div>
     <div class="container">
