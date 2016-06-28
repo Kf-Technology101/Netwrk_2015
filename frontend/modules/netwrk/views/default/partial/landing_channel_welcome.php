833bde32463fc278422a4c0b1bdebd254948838d
Shailesh <shailesh.bonzer@gmail.com>
2016-06-28 17:10:40 +0530

    Line break added

--- frontend/modules/netwrk/views/default/partial/landing_channel_welcome.php
+++ frontend/modules/netwrk/views/default/partial/landing_channel_welcome.php
@@ -8,7 +8,7 @@
 				</button>
 			</div>
 			<div class="modal-body">
-				<h4>Anyone can chat in a line. Housed in channels, they can be found or built anywhere. All we ask is...</h4>
+				<h4>Anyone can chat in a line.<br/> Housed in channels, they can be found or built anywhere.<br/> All we ask is...</h4>
 				<!--<h4>Click build to create your own, back to see the main channel, or join the area discussion here!</h4>
 				<h4>Whatever you do</h4>-->
 				<h1>Spread</h1>
