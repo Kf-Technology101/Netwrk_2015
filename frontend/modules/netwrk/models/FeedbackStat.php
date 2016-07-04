f8a31c753516488cab4041a29eac1e362c8bfec9
Shailesh <shailesh.bonzer@gmail.com>
2016-07-04 14:37:10 +0530

    Added feedback stat model

---
+++ frontend/modules/netwrk/models/FeedbackStat.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace frontend\modules\netwrk\models;
+
+use Yii;
+use yii\db\Query;
+
+/**
+ * This is the model class for table "feedback_stat".
+ *
+ * @property integer $id
+ * @property integer $points
+ * @property integer $ws_message_id
+ * @property string $type
+ */
+class FeedbackStat extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'feedback_stat';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['points'], 'required'],
+            [['type'], 'required']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'points' => 'Points',
+            'ws_message_id' => 'WS Message ID',
+            'type'  => 'Type'
+        ];
+    }
+}
