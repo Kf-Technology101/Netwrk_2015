f9c337b7f085245538841a09e300b22d95dcc572
Shailesh <shailesh.bonzer@gmail.com>
2016-07-06 17:48:43 +0530

    Landing and community modal feed chat section updates desktop

--- frontend/modules/netwrk/views/default/partial/landing_page.php
+++ frontend/modules/netwrk/views/default/partial/landing_page.php
@@ -57,9 +57,12 @@
 									<div class="post-content"><%= e.content%></div>
 								</div>
 								<div class="action">
-									<div class="chat"><i class="fa fa-comments"></i>Chat</div>
+									<div class="chat"><i class="fa fa-comments"></i>Jump in</div>
 
-									<span class="brilliant"><%= e.brilliant_count%></span>
+									<span class="chat feedback-wrapper">
+										<div class="feedback-line"></div>
+										<div class="feedback">F</div>
+									</span>
 								</div>
 							</div>
 						<%
@@ -124,9 +127,12 @@
 									<div class="post-content"><%= e.content%></div>
 								</div>
 								<div class="action">
-									<div class="chat"><i class="fa fa-comments"></i>Chat</div>
+									<div class="chat"><i class="fa fa-comments"></i>Jump in</div>
 
-									<span class="brilliant"><%= e.brilliant_count%></span>
+									<span class="chat feedback-wrapper">
+										<div class="feedback-line"></div>
+										<div class="feedback">F</div>
+									</span>
 								</div>
 							</div>
 						<%
