21d939e05b889e00857d6a3536a9caa73cac1015
Shailesh <shailesh.bonzer@gmail.com>
2016-07-07 13:46:23 +0530

    Mobile community console issue fixes

--- frontend/web/js/controller/topic.js
+++ frontend/web/js/controller/topic.js
@@ -641,11 +641,13 @@ var Topic = {
     },
 
     show_page_topic: function(city,params){
-        if (typeof params.group != "undefined"){
-            window.location.href = baseUrl + "/netwrk/topic/topic-page?city="+city+"&group="+params.group+"&name="+params.name+"&from="+params.from;
-        } else if(typeof params.zipcode != "undefined") {
-            window.location.href = baseUrl + "/netwrk/topic/topic-page?city="+city+"&zipcode="+params.zipcode+"&name="+params.name+"&lat="+params.lat+"&lng="+params.lng;
-        }else{
+        if(typeof params != "undefined") {
+            if(typeof params.group != "undefined") {
+                window.location.href = baseUrl + "/netwrk/topic/topic-page?city="+city+"&group="+params.group+"&name="+params.name+"&from="+params.from;
+            } else if(typeof params.zipcode != "undefined") {
+                window.location.href = baseUrl + "/netwrk/topic/topic-page?city="+city+"&zipcode="+params.zipcode+"&name="+params.name+"&lat="+params.lat+"&lng="+params.lng;
+            }
+        } else {
             window.location.href = baseUrl + "/netwrk/topic/topic-page?city="+city;
         }
     },
