79d1034b49c846557a2501233f64b508b35816f3
Shailesh <shailesh.bonzer@gmail.com>
2016-08-09 15:12:02 +0530

    Display on boarding modal once after sign up only

--- frontend/web/js/controller/map.js
+++ frontend/web/js/controller/map.js
@@ -147,9 +147,9 @@
 		    // Map.insertLocalGovernment();
 			//Map.requestBlueDotOnMap(lat,lng,Map.map);
 			Map.requestPosition(Map.map);
-			if(isMobile && sessionStorage.on_boarding == 1){
-				sessionStorage.on_boarding == 0;
 
+			if(sessionStorage.on_boarding == 1){
+				sessionStorage.on_boarding = 0;
 				// If user not have any lines and profile picture then display the modal
 				setTimeout(function(){
 					Login.showOnBoardingLines();
