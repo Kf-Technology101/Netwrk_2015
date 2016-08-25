d48f59a2be7efeeadebdec05ff164688606b1bf3
Shailesh <shailesh.bonzer@gmail.com>
2016-08-25 14:08:02 +0530

    Remove netwrk ui when netwrk news open

--- frontend/modules/netwrk/views/default/partial/netwrk_news.php
+++ frontend/modules/netwrk/views/default/partial/netwrk_news.php
@@ -6,59 +6,58 @@
 <div id='netwrkNews' class='netwrk-news'>
 	<div class="header-wrapper text-center"></div>
 	<div class="tab-wrapper">
-		<ul class="nav nav-tabs" role="tablist">
+		<!--<ul class="nav nav-tabs" role="tablist">
 			<li role="presentation" class="col-xs-4 active"><a href="#yourNetwrk" aria-controls="yourNetwrk" role="tab" data-toggle="tab"><span>Your Netwrk</span></a></li>
 			<li role="presentation" class="col-xs-4"><a href="#mostActive" aria-controls="mostActive" role="tab" data-toggle="tab"><span>Most Active</span></a></li>
 			<li role="presentation" class="col-xs-4"><a href="#publicCenters" aria-controls="publicCenters" role="tab" data-toggle="tab"><span>Public Centers</span></a></li>
-		</ul>
+		</ul>-->
 		<div class="tab-content content-wrapper">
 
 		</div>
 	</div>
 </div>
 <script id="netwrk_news" type="text/x-underscore-template">
-	<div role="tabpanel" class="tab-pane active" id="yourNetwrk">
-		<% if(!_.isEmpty(landing.feeds)) {%>
-			<% _.each(landing.feeds, function(city_feed, key){ %>
-				<% _.each(city_feed, function(e, key){ %>
-
-					<% if ((e.is_post == 1)){ %>
-						<div class="feed-row feed-post" data-user="<%= e.user_id %>" data-value="<%= e.id %>" data-city="<%= e.city_id %>" data-topic='<%= e.topic_id %>'>
-							<div class="avatar-poster"><div class="image"><img src="<%= e.photo %>"></div></div>
-							<div class="feed-content">
-								<div class='post'>
-									<div class='post-title'><%= e.title %></div>
-									<div class='post-content'><%= e.content %></div>
-									<span class='post-create-by'>Posted by: <%= e.posted_by %></span>
-									<span class='appear-day'>
-										<% if ((e.appear_day == 'Now')){ %>
-										  Just Now
-										<% }else{ %>
-										  <%= e.appear_day %> ago
-										<% } %>
-									</span>
-								</div>
-							</div>
-						</div>
-					<% }else{ %>
-						<div class="feed-row feed-topic fav-community-topic" data-value="<%= e.id %>" data-city="<%= e.city_id %>" data-city-name='<%= e.city_name %>'>
-							<div class="feed-content">
-								<span class='topic-title'><%= e.title %></span>
-								<span class='topic-create-by'>Channel created by: <%= e.created_by %></span>
+	<% if(!_.isEmpty(landing.feeds)) {%>
+		<% _.each(landing.feeds, function(city_feed, key){ %>
+			<% _.each(city_feed, function(e, key){ %>
+				<% if ((e.is_post == 1)){ %>
+					<div class="feed-row feed-post" data-user="<%= e.user_id %>" data-value="<%= e.id %>" data-city="<%= e.city_id %>" data-topic='<%= e.topic_id %>'>
+						<div class="avatar-poster"><div class="image"><img src="<%= e.photo %>"></div></div>
+						<div class="feed-content">
+							<div class='post'>
+								<div class='post-title'><%= e.title %></div>
+								<div class='post-content'><%= e.content %></div>
+								<span class='post-create-by'>Posted by: <%= e.posted_by %></span>
 								<span class='appear-day'>
 									<% if ((e.appear_day == 'Now')){ %>
-										Just Now
+									  Just Now
 									<% }else{ %>
-										<%= e.appear_day %> ago
+									  <%= e.appear_day %> ago
 									<% } %>
 								</span>
 							</div>
 						</div>
-					<% } %>
-				<% }); %>
+					</div>
+				<% }else{ %>
+					<div class="feed-row feed-topic fav-community-topic" data-value="<%= e.id %>" data-city="<%= e.city_id %>" data-city-name='<%= e.city_name %>'>
+						<div class="feed-content">
+							<span class='topic-title'><%= e.title %></span>
+							<span class='topic-create-by'>Channel created by: <%= e.created_by %></span>
+							<span class='appear-day'>
+								<% if ((e.appear_day == 'Now')){ %>
+									Just Now
+								<% }else{ %>
+									<%= e.appear_day %> ago
+								<% } %>
+							</span>
+						</div>
+					</div>
+				<% } %>
 			<% }); %>
-		<% } %>
-	</div>
+		<% }); %>
+	<% } %>
+	<!--<div role="tabpanel" class="tab-pane active" id="yourNetwrk">-->
+	<!--</div>
 	<div role="tabpanel" class="tab-pane" id="mostActive">
 		<%
 			var len_post = landing.top_post.length;
@@ -122,6 +121,6 @@
 				<%
 			});
 		%>
-	</div>
+	</div>-->
 </script>
 
--- frontend/web/js/controller/default.js
+++ frontend/web/js/controller/default.js
@@ -319,6 +319,9 @@ var Default ={
                     "left": ChatInbox.list_chat_post_right_hidden
                 }, 500);
             } else {
+                $(LandingPage.netwrk_news).animate({
+                    "left": "-400px"
+                }, 500);
                 $('#netwrkNavigation').animate({
                     "left": "0"
                 }, 500);
@@ -346,16 +349,19 @@ var Default ={
         target.unbind();
         target.on('click',function(){
             if(!$(this).hasClass('disabled')) {
-                if($(LandingPage.netwrk_news).css('left') == '100px'){
+                if($(LandingPage.netwrk_news).css('left') == '0px'){
                     $(LandingPage.netwrk_news).animate({
                         "left": "-400px"
                     }, 500);
-                    $(ChatInbox.chat_inbox).animate({
+                    /*$(ChatInbox.chat_inbox).animate({
                         "left": "100px"
-                    }, 500);
+                    }, 500);*/
                 } else {
                     $(LandingPage.netwrk_news).animate({
-                        "left": "100px"
+                        "left": "0px"
+                    }, 500);
+                    $('#netwrkNavigation').animate({
+                        "left": "-200px"
                     }, 500);
                     $(ChatInbox.chat_inbox).animate({
                         "left": ChatInbox.list_chat_post_right_hidden
