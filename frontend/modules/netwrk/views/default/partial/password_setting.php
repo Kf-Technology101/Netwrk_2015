28bb87998d596873b290de2f2d2a79a1060ee22a
Yogesh <yogesh.bonzer@gmail.com>
2016-08-31 15:22:08 +0530

    Added code to display password setting slider on desktop

--- frontend/modules/netwrk/views/default/partial/password_setting.php
+++ frontend/modules/netwrk/views/default/partial/password_setting.php
@@ -6,7 +6,7 @@
     use frontend\modules\netwrk\models\User;
     use frontend\modules\netwrk\models\PasswordSetting;
 ?>
-<div class="modal modal-profile" id='modal_password_setting'>
+<!--<div class="modal modal-profile" id='modal_password_setting'>
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -21,7 +21,7 @@
       </div>
       <div class="modal-body container">
         <?php
-          $form = ActiveForm::begin([
+/*          $form = ActiveForm::begin([
             'id' => 'password_setting_form',
             'options' => ['class' => 'form-password-setting form-horizontal'],
             'fieldConfig' => [
@@ -30,19 +30,19 @@
             ],
           ]);
           $user = new User(["scenario" => "password_setting"]);
-        ?>
+        */?>
           <div id="password_setting_success" class="alert alert-success hide"></div>
           <div class="field">
               <p class="title"> Current password </p>
-              <?= $form->field($user, 'currentPassword')->passwordInput(array('placeholder' => 'Current Password','autofocus'=>true)) ?>
+              <?/*= $form->field($user, 'currentPassword')->passwordInput(array('placeholder' => 'Current Password','autofocus'=>true)) */?>
           </div>
           <div class="field">
               <p class="title"> New password </p>
-              <?= $form->field($user, 'newPasswordProfile')->passwordInput(array('placeholder' => 'New Password')) ?>
+              <?/*= $form->field($user, 'newPasswordProfile')->passwordInput(array('placeholder' => 'New Password')) */?>
           </div>
           <div class="field">
               <p class="title"> Confirm password </p>
-              <?= $form->field($user, 'newPasswordConfirmProfile')->passwordInput(array('placeholder' => 'Confirm Password')) ?>
+              <?/*= $form->field($user, 'newPasswordConfirmProfile')->passwordInput(array('placeholder' => 'Confirm Password')) */?>
           </div>
           <div class="btn-control">
               <div class="reset pull-left">
@@ -53,8 +53,61 @@
               </div>
               <div class="clearfix"></div>
           </div>
-        <?php ActiveForm::end(); ?>
+        <?php /*ActiveForm::end(); */?>
       </div>
     </div>
   </div>
+</div>-->
+
+<div id="password_setting_slider">
+    <div class="slider-profile" id='modal_password_setting'>
+        <div class="slider-dialog">
+            <div class="slider-content">
+                <div class="slider-header">
+                    <div class="header">
+                        <div class="title-page">
+                            <span class="title">Password setting</span>
+                            <span class="slider-close-btn"><i class="fa fa-close"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="slider-body">
+                    <?php
+                    $form = ActiveForm::begin([
+                        'id' => 'password_setting_form',
+                        'options' => ['class' => 'form-password-setting form-horizontal'],
+                        'fieldConfig' => [
+                            'template' => "<div class=\"col-md-12\">{input}</div>\n<div class=\"col-md-12\">{error}</div>",
+                            'labelOptions' => ['class' => 'col-lg-2 control-label'],
+                        ],
+                    ]);
+                    $user = new User(["scenario" => "password_setting"]);
+                    ?>
+                    <div id="password_setting_success" class="alert alert-success hide"></div>
+                    <div class="field">
+                        <p class="title"> Current password </p>
+                        <?= $form->field($user, 'currentPassword')->passwordInput(array('placeholder' => 'Current Password','autofocus'=>true)) ?>
+                    </div>
+                    <div class="field">
+                        <p class="title"> New password </p>
+                        <?= $form->field($user, 'newPasswordProfile')->passwordInput(array('placeholder' => 'New Password')) ?>
+                    </div>
+                    <div class="field">
+                        <p class="title"> Confirm password </p>
+                        <?= $form->field($user, 'newPasswordConfirmProfile')->passwordInput(array('placeholder' => 'Confirm Password')) ?>
+                    </div>
+                    <div class="btn-control">
+                        <div class="reset pull-left">
+                            <p>Reset</p>
+                        </div>
+                        <div class="update pull-right">
+                            <p>Update</p>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <?php ActiveForm::end(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
