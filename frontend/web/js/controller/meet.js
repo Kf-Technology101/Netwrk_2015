9d5dff83cbf5bb553a67c3ae08fa29d36eeb7af8
Shailesh <shailesh.bonzer@gmail.com>
2016-09-07 18:13:01 +0530

    Meet functionality back on site

--- frontend/web/js/controller/meet.js
+++ frontend/web/js/controller/meet.js
@@ -51,8 +51,8 @@ var Meet ={
     },
 
     _init: function(){
-        Common.ShowModalComeBack();
-        return;
+        //Common.ShowModalComeBack();
+        //return;
         var post_id = Meet.getParameterByName('post_id'),
             user_view = Meet.getParameterByName('user_id'),
             from = Meet.getParameterByName('from');
